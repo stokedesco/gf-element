eaa8252c12958924cb65c0f283b98f4091a38aca
Leigh Ericksen <109568123+stokedesco@users.noreply.github.com>
2025-09-07 16:22:23 +1000

    Push Main Development (#23)
    * Add Elementor group control imports
    
    * Remove theme selection control (#2)
    
    * Simplify render wrapper class (#3)
    
    * Add master style section toggle (#4)
    
    * Add label typography and color controls (#5)
    
    * Add heading style controls (#6)
    
    * Add input styling controls (#8)
    
    * feat: add textarea styling controls (#9)
    
    * Add checkbox and radio style controls (#10)
    
    * Add file upload button styling controls (#11)
    
    * Add progress bar styling controls (#12)
    
    * Add style controls for Gravity Forms buttons (#13)
    
    * Add message styling controls (#14)
    
    * Add field spacing controls (#15)
    
    * Refine label controls styling (#7)
    
    * Update gravity-forms-elementor-widget.php
    
    * Wrap heading controls in a section (#17)
    
    * feat: enhance element styling controls and rebrand (#18)
    
    * Rename plugin to Stoke GF Elementor
    
    * fix plugin header file name (#21)
    
    * Avoid fatal error when Elementor is missing (#22)
    
    * Update stoke-gf-elementor.php

---
+++ stoke-gf-elementor.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Plugin Name:         Stoke GF Elementor
+ * Plugin URI:          https://stokedesign.co/sandbox
+ * Description:         Allows Gravity forms to easily be inserted and styled in Elementor.
+ * Version:             1.0.0
+ * Author:              Stoke Design Co
+ * Author URI:          https://stokedesign.co/
+ * Text Domain:         stoke-gf-elementor
+ * Requires at least:   4.7
+ * License:             GPLv3 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+namespace StokeGFElementor;
+
+use GFCommon;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+const MIN_GF_VERSION = '2.7.15';
+
+/**
+ * Check if Gravity Forms is installed and activated, and if the minimum required version is met.
+ *
+ * @since 1.0.0
+ *
+ * @return bool True if Gravity Forms is installed and activated, and the minimum required version is met. False otherwise.
+ */
+function meets_gravity_forms_requirements() {
+	return class_exists( 'GFCommon' ) && version_compare( GFCommon::$version, MIN_GF_VERSION, '>' );
+}
+
+/**
+ * Register the Elementor widget.
+ */
+add_action(
+    'elementor/widgets/register',
+    function ( $widgets_manager ) {
+                if ( ! meets_gravity_forms_requirements() ) {
+                        return;
+                }
+
+                require_once plugin_dir_path( __FILE__ ) . 'includes/Widget.php';
+
+                // Register widget.
+                $widgets_manager->register( new Widget() );
+
+                add_action(
+                        'elementor/editor/after_enqueue_styles',
+                        array( Widget::class, 'enqueue_editor_styles' )
+                );
+        }
+);
+
+/*
+ * Display a notice if Gravity Forms is not installed or the minimum required version is not met.
+ */
+add_action(
+    'admin_notices',
+    function () {
+		if ( meets_gravity_forms_requirements() ) {
+			return;
+		}
+
+		$notice = wpautop(
+            strtr(
+            // translators: Do not translate [version]; it is replaced with the minimum required Gravity Forms version required.
+                esc_html__( '[plugin_name] requires Gravity Forms [version] or higher. Please upgrade Gravity Forms to use this widget.', 'stoke-gf-elementor' ),
+                array(
+					'[plugin_name]' => esc_html__( 'Stoke GF Elementor', 'stoke-gf-elementor' ),
+					'[version]'     => MIN_GF_VERSION,
+                )
+            )
+		);
+
+		echo "<div class='error is-dismissible' style='padding: 1.25em 0 1.25em 1em;'>$notice</div>"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+);
